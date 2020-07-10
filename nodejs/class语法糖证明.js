1ceddb625eaca4b30b0c4b987009f3226c3ec328
lvpeng <1217869072@qq.com>
2020-07-10 20:39:54 +0800

    class语法糖证明

---
+++ nodejs/class语法糖证明.js
@@ -0,0 +1,29 @@
+class Hello {
+    constructor() {
+        this.a = "aaa"
+    }
+    say() {
+        console.log(this.a)
+    }
+}
+// var Hello = function() {
+//     function Hello() {
+//         this.a = "aaa"
+//     }
+//     Hello.prototype.say = function() {
+//         console.log(this.a)
+//     }
+//     return Hello
+// }()
+
+
+console.log(Hello.prototype)
+var hello = new Hello()
+
+console.log(hello)
+
+hello.say()
+console.log(hello.constructor === Hello)
+console.log(hello.__proto__ === Hello.prototype)
+console.log(hello.__proto__.constructor === Hello)
+
