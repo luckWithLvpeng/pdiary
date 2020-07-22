d9d98cedfb9a546a924e576fb4706267a02c9c9e
lvpeng <1217869072@qq.com>
2020-07-22 15:58:44 +0800

    选择排序java

---
+++ 算法/排序/冒泡/src/Test.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+ * descriptionTODO
+ * <p>时间 2020/7/22 - 3:38 下午</p>
+ *
+ * @author 吕蓬
+ * @version 1.0
+ */
+public class Test {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length -1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            if (minIndex != i) {
+                swap(arr,i, minIndex);
+            }
+        }
+    }
+    public static void swap(int[] arr, int i,int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,5,8,1,9};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
